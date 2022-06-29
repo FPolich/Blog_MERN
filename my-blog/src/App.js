60a1835067de4fafc7d4fa098731db239e5f9a0e
Frann <franciscopolich@gmail.com>
2022-06-29 15:44:55 -0300

    agregado navbar

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+//Pages
 import Home from './pages/Home';
 import Article from './pages/Article';
 import ArticleList from './pages/ArticlesList';
 import About from './pages/About';
 
+//Components 
+import NavBar from "./components/NavBar";
+
+
 function App() {
   return (
         <BrowserRouter>
+        <NavBar />
         <div className="max-w-screen-md mx-auto pt-20">
           <h1>Hello, React Router!</h1>
           <Routes>
